919d7a0a111ed2aa6343bb0281695d438da5b2f6
filip <filip.kozjak@gmail.com>
2017-10-11 17:24:16 +0200

    Created class skelets for the GLA and LA

--- src/GLA.java
+++ src/GLA.java
@@ -1,10 +1,52 @@
+import java.io.InputStream;
 
-
+/**
+ * <b>Generator of Lexical Analyzer.</b> <br>
+ * 
+ * This class is used for generating the <b>Lexical Analyzer</b> 
+ * based on it's definitions. <br>
+ * 
+ * As per current version, it reads in the definitions stream, creates
+ * automatons and writes them to the file that will be read by the <b>Lexical Analyzer</b>.
+ * 
+ * 
+ * @author fiilip
+ *
+ */
 public class GLA {
 
+	/**
+	 * Program entry - point.
+	 * 
+	 * @param args	Not used.
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
+	/**
+	 * Source of the input file containing the definitions.<br>
+	 * Possible sources: System.in, FileInputStream, etc.
+	 */
+	private final InputStream istream;
+	
+	/**
+	 * Public constructor.<br>
+	 * Constructs the new {@link GLA} object.
+	 * 
+	 * @param istream	Stream of the input definitions.
+	 */
+	public GLA(InputStream istream) {
+		this.istream = istream;
+	}
 
+	/**
+	 * Generates the objects needed by the {@link LA}.<br>
+	 * 
+	 * In terms, generates the automatons used by the <b>Lexical Analyzer</b>.
+	 */
+	public void generateLA() {
+		
+	}
 }
